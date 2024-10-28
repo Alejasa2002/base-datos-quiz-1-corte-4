f63e234752fc838beb552bab528114b6a254962a
Alejasa2002 <lsanchez-2022a@corhuila.edu.co>
2024-10-28 09:56:05 -0500

    Update Caso1_sinfiltro.sql

--- Caso 1/Caso1_sinfiltro.sql
+++ Caso 1/Caso1_sinfiltro.sql
@@ -12,7 +12,7 @@ FROM
     role_module rm ON r.id = rm.role_id
         INNER JOIN
     module m ON rm.module_id = m.id
-        INNER JOIN
+        INNER JOIN 
     module_view mv ON m.id = mv.module_id
         INNER JOIN
     view v ON mv.view_id = v.id
